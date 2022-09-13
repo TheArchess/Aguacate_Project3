8bbe57a778941368fe6f1b0780d7da7b87ec36fa
Live session user <juanavalentinarico@gmail.com>
2022-09-13 09:48:00 -0500

    ejericio1

--- task2.cpp
+++ task2.cpp
@@ -44,16 +44,13 @@ void task2()
             {
                 digitalWrite(led, HIGH);
 
-                Serial.print("1");
-                Serial.print('\n');
-
+                Serial.print("1\n");
             }
             else if (command == "outOFF")
             {
                 digitalWrite(led, LOW);
 
-                Serial.print("0");
-                Serial.print('\n');
+                Serial.print("0\n");
             }
             else if (command == "read")
             {
@@ -88,3 +85,122 @@ void task2()
     }
     }
 }
+/*
+using UnityEngine;
+using System.IO.Ports;
+using TMPro;
+
+enum TaskState
+{
+    INIT,
+    WAIT_COMMANDS
+}
+public class Serial : MonoBehaviour
+{
+    private static TaskState taskState = TaskState.INIT;
+    private SerialPort _serialPort;
+    private byte[] buffer;
+    public TextMeshProUGUI myText;
+    public TextMeshProUGUI myButton;
+    public TextMeshProUGUI myLed;
+    private int counter = 0;
+    public static bool btnOn = false;
+    public static bool btnOff = false;
+    public static bool btnread = false;
+    void Start()
+    {
+        _serialPort = new SerialPort();
+        _serialPort.PortName = "/dev/ttyACM0";
+        _serialPort.BaudRate = 115200;
+        _serialPort.DtrEnable = true;
+        _serialPort.NewLine = "\n";
+        _serialPort.Open();
+        Debug.Log("Open Serial Port");
+        buffer = new byte[128];
+    }
+
+    void Update()
+    {
+        //Codigo contador
+        myText.text = counter.ToString();
+        counter++;
+
+        switch (taskState)
+        {
+            case TaskState.INIT:
+                taskState = TaskState.WAIT_COMMANDS;
+                Debug.Log("WAIT COMMANDS");
+                break;
+            case TaskState.WAIT_COMMANDS:
+                
+                if (btnOn==true)
+                {
+                    _serialPort.Write("outON\n");
+                    Debug.Log("Send outON");
+
+                    btnOn = false;
+                }
+                if (btnOff==true)
+                {
+                    _serialPort.Write("outOFF\n");
+                    Debug.Log("Send outOFF");
+
+                    btnOff = false;
+                }
+
+                if (btnread==true)
+                {
+                    _serialPort.Write("read\n");
+                    Debug.Log("Send read");
+                    btnread = false;
+                }
+                
+                if (_serialPort.BytesToRead > 0)
+                {
+                    string response = _serialPort.ReadLine();
+
+                    if (response.Remove('\n') == "0")
+                    {
+                        myLed.text = response.ToString();
+                    }
+                    if (response.Remove('\n') == "1")
+                    {
+                        myLed.text = response.ToString();
+                    }
+                    else
+                    {
+                        myButton.text = response.ToString();
+                        myLed.text = response.ToString();
+                    }
+                    
+                   
+                    break;
+                }
+
+                break;
+            default:
+                Debug.Log("State Error");
+                break;
+        }
+    }
+    
+    //Botón-OnClick
+    public void EventButtonReadSend()
+    {
+        Debug.Log("Button Read Pressed");
+        btnread = true;
+    }
+    public void EventButtonOFFSend()
+    {
+        Debug.Log("Button OFF Pressed");
+        btnOff = true;
+    }
+    public void EventButtonONSend()
+    {
+        Debug.Log("Button ON Pressed");
+        btnOn = true;
+    }
+}
+
+
+*/
